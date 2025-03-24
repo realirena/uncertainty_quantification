992660b481924cf7822a74caefbc4ebaf5a4ca7f
irena <irena@umich.edu>
2025-03-24 14:01:03 -0500

    add lifetables for sensitivity check

--- R/model/get_le0_estimates.R
+++ R/model/get_le0_estimates.R
@@ -11,7 +11,7 @@ source("R/0_setup.R")
 ## set the working directory
 setwd("U:/Documents/repos/uncertainty_quantification")
 model_dir <- paste0(getwd(),"/R/model/")
-results_dir <- paste0(getwd(),"/R/sensitivity_check/samples/palestine_bu/")
+results_dir <- paste0(getwd(),"/R/sensitivity_check/samples/gaza_bu/")
 
 le0_noc <- readRDS("data/ex0_noc.rds")
 le0_noc_23 <- le0_noc[le0_noc$year==2023&le0_noc$source=="lc_pcbs_2019"&le0_noc$region=="Gaza Strip",]
@@ -31,7 +31,7 @@ le_noc_list <- list(
 le0_gaza_mean <- c(78.29336,  74.95395, 76.67211)
 
 ## read in samples 
-file_names <- c("moh_24_samples_1", "moh_24_samples_2", "moh_24_samples_3", "moh_24_samples_4")
+file_names <- c("un_geno24_samples_1", "un_geno24_samples_2", "un_geno24_samples_3", "un_geno24_samples_4")
 model_out <- read_stan_csv(paste0(results_dir, file_names,".csv"))
 
 ### extract the model-generated mortality distributions (incl WPP deaths)
@@ -69,9 +69,9 @@ all_lifetable_m <- Reduce(rbind,lifetable_m)
 all_lifetable_t <- Reduce(rbind,lifetable_t)
 
 ## scenarios:  "GMoH report", "B'Tselem historical average", "UN-IGME pattern"
-lifetable_f_age0 <- get_le0_dt(all_lifetable_f, "Females", 2024,  "GMoH report", le0= le_noc_list[[4]])
-lifetable_m_age0 <- get_le0_dt(all_lifetable_m, "Males", 2024,  "GMoH report",le0= le_noc_list[[4]])
-lifetable_t_age0 <- get_le0_dt(all_lifetable_t, "Total", 2024, "GMoH report",le0= le_noc_list[[4]])
+lifetable_f_age0 <- get_le0_dt(all_lifetable_f, "Females", 2024, "UN-IGME pattern", le0= le_noc_list[[2]])
+lifetable_m_age0 <- get_le0_dt(all_lifetable_m, "Males", 2024,  "UN-IGME pattern",le0= le_noc_list[[2]])
+lifetable_t_age0 <- get_le0_dt(all_lifetable_t, "Total", 2024,  "UN-IGME pattern",le0= le_noc_list[[2]])
 
 ## histograms of the estimated life expectancies after accounting for reporting rate error 
 hist(lifetable_f_age0$ex)
@@ -109,7 +109,7 @@ g3 <- ggplot(data=lifetable_t_age0, aes(x=ex)) +
 
 gridExtra::grid.arrange(g1, g2, g3, ncol=3)
 
-write.csv(lifetable_m_age0, paste0(results_dir, "moh_2024_lifetable_m_le0.csv"), row.names = FALSE)
-write.csv(lifetable_f_age0, paste0(results_dir, "moh_2024_lifetable_f_le0.csv"), row.names = FALSE)
-write.csv(lifetable_t_age0, paste0(results_dir, "moh_2024_lifetable_t_le0.csv"), row.names = FALSE)
+write.csv(lifetable_m_age0, paste0(results_dir, "un_geno24_lifetable_m_le0.csv"), row.names = FALSE)
+write.csv(lifetable_f_age0, paste0(results_dir, "un_geno24_lifetable_f_le0.csv"), row.names = FALSE)
+write.csv(lifetable_t_age0, paste0(results_dir, "un_geno24_lifetable_t_le0.csv"), row.names = FALSE)
 
